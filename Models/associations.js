c20dcd38babc76fc8564d4ef4d843780a26ad916
Kevin Latino <111583322+KevinLatino@users.noreply.github.com>
2024-07-02 08:47:53 -0600

    adding an association 1 to 1 between user and score

--- Models/associations.js
+++ Models/associations.js
@@ -2,14 +2,17 @@ import { User } from "./users.model.js"
 import { Task } from "./tasks.model.js"
 import { Medal } from "./medals.model.js";
 import { UserMedal } from "./users-medals.model.js";
+import { Score } from "./score.model.js";
 
 const initAssociations = () => {
 
-    //A user can have many Tasks, but a task just can have a user
+    //A score just can have a user and a user just can have a score
+    Score.belongsTo(User, { as: "user" });
+
+
+    //A user can have many Tasks but a task just can have a user
     User.hasMany(Task, { as: "task", foreignKey: "userId" });
     Task.belongsTo(User, { as: "user" });
-
-
 
     //A user can have many Medals and a medal can have many users
     User.belongsToMany(Medal, { as: "medal", through: UserMedal, foreignKey: "userId", otherKey: "medalID" });

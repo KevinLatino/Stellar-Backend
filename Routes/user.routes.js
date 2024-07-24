6a98fe26fc71b3719528b9160b0860c0bd94f8ca
Kevin Latino <111583322+KevinLatino@users.noreply.github.com>
2024-07-23 20:54:34 -0600

    minimal functions

--- Routes/user.routes.js
+++ Routes/user.routes.js
@@ -1,29 +1,34 @@
 import { Router } from "express";
 import UserService from "../Services/user.service.js";
+import passport from "passport";
 
 const userServices = new UserService();
 const router = Router();
 
-router.post('/create', async (req, res, next) => {
-    try {
-        const userData = req.body
-        const newUser = await userServices.createUser(userData);
-        res.json(newUser)
-    } catch (error) {
-        next(error)
-    }
-})
+router.post('/create',
+    passport.authenticate("jwt", { session: false }),
+    async (req, res, next) => {
+        try {
+            const userData = req.body
+            const newUser = await userServices.createUser(userData);
+            res.json(newUser)
+        } catch (error) {
+            next(error)
+        }
+    })
 
-router.put('/update/:id', async (req, res, next) => {
-    try {
-        const { id } = req.params
-        const userData = req.body;
-        const update = await userServices.updateUser(id, userData)
-        res.json(update)
-    } catch (error) {
-        next(error)
-    }
-})
+router.put('/update/:id',
+    passport.authenticate("jwt", { session: false }),
+    async (req, res, next) => {
+        try {
+            const { id } = req.params
+            const userData = req.body;
+            const update = await userServices.updateUser(id, userData)
+            res.json(update)
+        } catch (error) {
+            next(error)
+        }
+    })
 
 
 export default router

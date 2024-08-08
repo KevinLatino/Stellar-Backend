528dfd57a1b7280a60be909b6837927b9ae0ddd6
Kevin Latino <111583322+KevinLatino@users.noreply.github.com>
2024-08-07 18:14:23 -0600

    adding new services to medal.service.js

--- Services/medal.service.js
+++ Services/medal.service.js
@@ -2,9 +2,23 @@ import { Medal } from "../Models/medals.model";
 
 class MedalService {
 
-    async createMedal (medalBody) {
+    async findById(id) {
+        const foundId = await Medal.findByPk(id);
+        return foundId
+    }
+
+    async createMedal(medalBody) {
         const createMedal = await Medal.create(medalBody);
         return createMedal
+    }
+
+    async findMedal(id) {
+        const medal = await Medal.findOne({
+            where: {
+                id: id
+            }
+        })
+        return medal
     }
 }
 

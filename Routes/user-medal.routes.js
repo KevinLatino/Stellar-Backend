80749e261b717d3918bcd6089a15bab379d21c75
Kevin Latino <111583322+KevinLatino@users.noreply.github.com>
2024-08-15 13:45:38 -0600

    adding new endpoints

--- Routes/user-medal.routes.js
+++ Routes/user-medal.routes.js
@@ -11,5 +11,35 @@ router.post('/goals', async (req, res) => {
     res.json(goalsMedal)
 })
 
+router.post('/Eisenhower', async (req, res) => {
+    const { userId, medalId } = req.body
+    const goalsMedal = await userMedalServices.addEisenhowerMedal(userId, medalId);
+    res.json(goalsMedal)
+})
+
+router.post('/Pomodoro', async (req, res) => {
+    const { userId, medalId } = req.body
+    const goalsMedal = await userMedalServices.addPodomoroMedal(userId, medalId);
+    res.json(goalsMedal)
+})
+
+router.post('/Dates', async (req, res) => {
+    const { userId, medalId } = req.body
+    const goalsMedal = await userMedalServices.addDatesMedal(userId, medalId);
+    res.json(goalsMedal)
+})
+
+router.post('/Environment', async (req, res) => {
+    const { userId, medalId } = req.body
+    const goalsMedal = await userMedalServices.addEnvironmentMedal(userId, medalId);
+    res.json(goalsMedal)
+})
+
+router.post('/Mindfulness', async (req, res) => {
+    const { userId, medalId } = req.body
+    const goalsMedal = await userMedalServices.addMindfulnessMedal(userId, medalId);
+    res.json(goalsMedal)
+})
+
 
 export default router;
